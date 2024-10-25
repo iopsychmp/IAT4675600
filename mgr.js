5359e81bb6f2a320ef06701f2d2d30ea6a327b90
iopsychmp <169057695+iopsychmp@users.noreply.github.com>
2024-10-25 17:14:51 -0400

    Update mgr.js
    why isn't data pipe working

--- mgr.js
+++ mgr.js
@@ -10,7 +10,7 @@ define(['managerAPI',
 
 var API    = new Manager();
 //const subid = Date.now().toString(16)+Math.floor(Math.random()*10000).toString(16);
-init_data_pipe(API, 'y1vWLQb3pdKY',  {file_type:'csv'});	
+init_data_pipe(API, 'EfBQoXQnOeFT',  {file_type:'csv'});	
 
 API.setName('mgr');
 API.addSettings('skip',true);
