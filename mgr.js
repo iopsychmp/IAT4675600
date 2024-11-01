90ea1265d061d1659bdfbd9e04a5e4232e4bdb24
iopsychmp <alexis.collins.9412@gmail.com>
2024-10-31 23:00:17 -0400

    Update mgr.js
    Updated redirect link to for qualtrics study version 17

--- mgr.js
+++ mgr.js
@@ -129,7 +129,7 @@ API.addTasksSet({
     redirect:
     [{ 
         //Replace with any URL you need to put at the end of your study, or just remove this task from the sequence below
-        type:'redirect', name:'redirecting', url: `https://uwaterloo.ca1.qualtrics.com/jfe/form/SV_9v6keJcB7RFH7z8?respondentID=${respondentID}&redirect=1` 
+        type:'redirect', name:'redirecting', url: `https://uwaterloo.ca1.qualtrics.com/jfe/form/SV_0cRXcu1eJMYGY7A?respondentID=${respondentID}&Q_R=${respondentID}&redirect=1` 
     }],
     
     //This task waits until the data are sent to the server.
